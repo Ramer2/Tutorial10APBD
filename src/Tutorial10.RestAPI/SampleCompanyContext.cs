b6b7f949bc453e3e1febfb8bae6ae9044605b350
s30422 <sisoev2006a@gmail.com>
2025-05-16 11:11:12 +0200

    models

---
+++ src/Tutorial10.RestAPI/SampleCompanyContext.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Tutorial10.RestAPI;
+
+public partial class SampleCompanyContext : DbContext
+{
+    public SampleCompanyContext()
+    {
+    }
+
+    public SampleCompanyContext(DbContextOptions<SampleCompanyContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Departemnt> Departemnts { get; set; }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    public virtual DbSet<Job> Jobs { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema("s30422");
+
+        modelBuilder.Entity<Departemnt>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Departem__3214EC07397A32B1");
+
+            entity.ToTable("Departemnt");
+
+            entity.HasIndex(e => e.Name, "UQ__Departem__737584F64D29A3F5").IsUnique();
+
+            entity.Property(e => e.Location)
+                .HasMaxLength(33)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(25)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Employee__3214EC07BDEFE134");
+
+            entity.ToTable("Employee");
+
+            entity.Property(e => e.Commission).HasColumnType("decimal(7, 2)");
+            entity.Property(e => e.Name)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.Salary).HasColumnType("decimal(7, 2)");
+
+            entity.HasOne(d => d.Department).WithMany(p => p.Employees)
+                .HasForeignKey(d => d.DepartmentId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Employee__Depart__0997B173");
+
+            entity.HasOne(d => d.Job).WithMany(p => p.Employees)
+                .HasForeignKey(d => d.JobId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Employee__JobId__07AF6901");
+
+            entity.HasOne(d => d.Manager).WithMany(p => p.InverseManager)
+                .HasForeignKey(d => d.ManagerId)
+                .HasConstraintName("FK__Employee__Manage__08A38D3A");
+        });
+
+        modelBuilder.Entity<Job>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Job__3214EC0793D8C020");
+
+            entity.ToTable("Job");
+
+            entity.HasIndex(e => e.Name, "UQ__Job__737584F6E9035870").IsUnique();
+
+            entity.Property(e => e.Name)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
