5a5064e6b924c8c44a90e2bb7a517475edbf7316
s30422 <sisoev2006a@gmail.com>
2025-05-16 11:11:04 +0200

    lasdjfklasdfklaskldfjklasjdf

--- src/Tutorial10.RestAPI/Program.cs
+++ src/Tutorial10.RestAPI/Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Tutorial10.RestAPI;
+using Tutorial10.RestAPI.DTOs;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("EmployeeDatabase");
+
+builder.Services.AddDbContext<SampleCompanyContext>(options => options.UseSqlServer(connectionString));
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -16,20 +24,58 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("/api/jobs", () => {
-    
+app.MapGet("/api/jobs", async (SampleCompanyContext context, CancellationToken cancellationToken) => {
+    try
+    {
+        return Results.Ok(await context.Jobs.ToListAsync(cancellationToken));
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem(ex.Message);
+    }
 });
 
-app.MapGet("/api/departments", () => {
-    
+app.MapGet("/api/departments", async (SampleCompanyContext context, CancellationToken cancellationToken) => {
+    try
+    {
+        return Results.Ok(await context.Departemnts.ToListAsync(cancellationToken));
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem(ex.Message);
+    }
 });
 
-app.MapGet("/api/employees", () =>
-{
-    
+app.MapGet("/api/employees", async (SampleCompanyContext context, CancellationToken cancellationToken) =>
+{   
+    try
+    {
+        var employees = await context.Employees.ToListAsync(cancellationToken);
+        var employeeDtos = new List<EmployeeDTO>();
+        foreach (var employee in employees)
+        {
+            employeeDtos.Add(new EmployeeDTO
+            {
+                Id = employee.Id,
+                Name = employee.Name,
+                JobId = employee.JobId,
+                HireDate = employee.HireDate,
+                Salary = employee.Salary,
+                Commission = employee.Commission,
+                DepartmentId = employee.DepartmentId
+            });
+        }
+        
+        return Results.Ok(employeeDtos);
+
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem(ex.Message);
+    }
 });
 
-app.MapGet("/api/employees/{id}", (int id) =>
+app.MapGet("/api/employees/{id}", async (SampleCompanyContext context, CancellationToken cancellationToken, int id) =>
 {
     
 });
---
+++ src/Tutorial10.RestAPI/dtos/EmployeeDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace Tutorial10.RestAPI.DTOs;
+
+public class EmployeeDTO
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public int JobId { get; set; }
+
+    public DateTime HireDate { get; set; }
+
+    public decimal Salary { get; set; }
+
+    public decimal? Commission { get; set; }
+
+    public int DepartmentId { get; set; }
+}
